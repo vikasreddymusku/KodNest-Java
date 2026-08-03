71f07515f9984fe113fd8939cb6299ecfc10f1b6
Vikas Reddy <vikasreddymusku@gmail.com>
2026-08-03 12:18:08 +0530

    M01-T01-ST01-L: begin the Java track

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,12 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Java Track!");
+        System.out.println("Welcome to the Java Track.");
+        System.out.println("I already know how to build programming logic.");
+        System.out.println("Now I am learning how to express that logic using Java.");
+
+        System.out.println("My name is Musku Vikas Reddy.");
+        System.out.println("My career goal is to become a Software Developer.");
+        System.out.println(
+                "I selected Java because I want to build strong programming skills and develop real-world applications.");
     }
 }
